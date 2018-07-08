e0d67ba888cbc44adf1fa4d1aa7d143b813f75b5
q1002152643 <1002152643@qq.com>
2018-07-08 20:22:25 +0800

    新增嗲吗

--- src/main/java/com/yooli/demo/App.java
+++ src/main/java/com/yooli/demo/App.java
@@ -1,11 +1,20 @@
 package com.yooli.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by PY on 2018/7/7.
  */
 public class App {
 
     public static void main(String[] args) {
-
+        List<User> lists=new ArrayList<>();
+        for (int i=0;1<10;i++){
+            lists.add(new User(i,"name"+String.valueOf(i)));
+            System.out.println(lists.get(i));
+        }
+        int i=5;
+        System.out.println(i);
     }
 }
